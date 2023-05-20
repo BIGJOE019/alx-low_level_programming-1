92ed660e76793f9e300f2a0491b0d6dfc58a6b57
BIGJOE <125502618+BIGJOE019@users.noreply.github.com>
2023-05-20 17:02:52 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,5 +7,5 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 %s\n")
     return (1);
